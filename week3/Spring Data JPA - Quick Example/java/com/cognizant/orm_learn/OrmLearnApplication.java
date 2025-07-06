daf0d5f7735fdccf25dfa8109be3d9b5bc101d1b
NavyaVathumilli <vathumillinavya@gmail.com>
2025-07-07 00:13:13 +0530

    Adding week-3 files

---
+++ week3/Spring Data JPA - Quick Example/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -0,0 +1,43 @@
+package com.cognizant.orm_learn;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.service.CountryService;
+
+@ComponentScan(basePackages = "com.cognizant.orm_learn")
+@EnableTransactionManagement
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+    private static CountryService countryService;
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+        LOGGER.info("Inside main");
+
+        countryService = context.getBean(CountryService.class);
+        testGetAllCountries();
+    }
+
+    private static void testGetAllCountries() {
+        try {
+            LOGGER.info("Start");
+            List<Country> countries = countryService.getAllCountries();
+            LOGGER.debug("countries={}", countries);
+            System.out.println("Countries: " + countries);
+            LOGGER.info("End");
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
